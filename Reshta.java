63130fb7a1a7e9470d236ab1d7f6ef5c7d02c53e
skajenavchitelka <145810747+skajenavchitelka@users.noreply.github.com>
2023-09-23 18:25:07 +0000

    Reshta

---
+++ Reshta.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Reshta {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the currency rate");
+        double currency = sc.nextDouble();
+        System.out.println("Enter amount of money");
+        int hryvna = sc.nextInt();
+
+        int amount = convert(currency, hryvna);
+        double change = getChange(currency, hryvna);
+        print(amount, change);
+    }
+
+    static int convert(double currency, int hryvna) {
+        return ((int) (hryvna / currency));
+    }
+
+    static double getChange(double currency, int hryvna) {
+        return hryvna - convert(currency, hryvna) * currency;
+    }
+
+    static void print(int amount, double change) {
+        System.out.println("You can byu " + amount);
+        if (change != 0) {
+            System.out.printf("You rest of money is %.2f", change);
+            System.out.println();
+
+        }
+
+    }
+
+}
